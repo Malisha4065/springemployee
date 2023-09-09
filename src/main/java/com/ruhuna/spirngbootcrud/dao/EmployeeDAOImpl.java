add32da2231bf1637f26350c20fc3652a9c3606c
Malisha A.P.D <128911037+Malisha4065@users.noreply.github.com>
2023-09-09 16:49:14 +0530

    initial

---
+++ src/main/java/com/ruhuna/spirngbootcrud/dao/EmployeeDAOImpl.java
@@ -0,0 +1,46 @@
+package com.ruhuna.spirngbootcrud.dao;
+
+import com.ruhuna.spirngbootcrud.dao.EmployeeDAO.IEmployeeDAO;
+import com.ruhuna.spirngbootcrud.entities.Employee;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.TypedQuery;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+@Repository
+public class EmployeeDAOImpl implements IEmployeeDAO {
+
+    private EntityManager entityManager;
+
+    @Autowired
+    public EmployeeDAOImpl(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
+
+    @Override
+    public List<Employee> findAll() {
+        TypedQuery<Employee> typedQuery = entityManager.createQuery("from Employee", Employee.class);
+        List<Employee> employeeList = typedQuery.getResultList();
+        return employeeList;
+    }
+
+    @Override
+    public Employee findById(int id) {
+        Employee employee = entityManager.find(Employee.class, id);
+        return null;
+    }
+
+    @Override
+    public Employee save() {
+        return null;
+    }
+
+    @Override
+    public void deleteById(int id) {
+
+    }
+
+
+}
---
+++ src/main/java/com/ruhuna/spirngbootcrud/rest/EmployeeRestController.java
@@ -0,0 +1,34 @@
+package com.ruhuna.spirngbootcrud.rest;
+
+import com.ruhuna.spirngbootcrud.dao.EmployeeDAO.IEmployeeDAO;
+import com.ruhuna.spirngbootcrud.entities.Employee;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RequestMapping("api")
+@RestController
+public class EmployeeRestController {
+    private IEmployeeDAO employeeDAO;
+
+    @Autowired
+    //constructor injection
+    public EmployeeRestController(IEmployeeDAO employeeDAO) {
+        this.employeeDAO = employeeDAO;
+    }
+    //localhost:8080/api/employees
+    @GetMapping("/employees")
+    List<Employee> findAll() {
+        System.out.println("sadas");
+        return employeeDAO.findAll();
+    }
+
+    @GetMapping("employees/{requestedID}")
+    Employee findEmployeeById(@PathVariable int requestedID) {
+        return employeeDAO.findById(requestedID);
+    }
+}
